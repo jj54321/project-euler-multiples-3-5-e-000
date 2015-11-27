fc16ac6835fd17a2532ce8d923561c2a1f208b07
jj54321 <mailme711@gmail.com>
2015-11-27 18:10:17 +0000

    nitrous-backup at Fri Nov 27 18:10:17 UTC 2015

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  for i in 0..limit do
+    multiples = []
+   if i%3 == 0 || i%5 == 0
+     multiples.push(i)
+   end
+end
+  return multiples
+end
+
+
